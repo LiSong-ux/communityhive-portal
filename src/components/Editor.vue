94f24a3f5b7de919621b28cd6a4f36108d877ab4
未央 <hiveforwork@163.com>
2019-10-27 21:34:58 +0800

    引入wangEditor富文本编辑器，发表帖子页面完成！

---
+++ src/components/Editor.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="edit_container" ref="editor"></div>
+        <!--<button v-on:click="getContent">查看内容</button>-->
+        <Button type="primary" size="large" v-on:click="getContent">{{ label }}</Button>
+    </div>
+</template>
+
+<script>
+    import E from 'wangeditor'
+
+    export default {
+        name: 'editor',
+        props: {
+            label: String
+        },
+        data() {
+            return {
+                editorContent: ''
+            }
+        },
+        methods: {
+            getContent: function () {
+                alert(this.editorContent)
+            }
+        },
+        mounted() {
+            var editor = new E(this.$refs.editor)
+            editor.customConfig.onchange = (html) => {
+                this.editorContent = html
+            }
+            editor.create()
+        }
+    }
+</script>
+
+<style scoped>
+    .edit_container {
+        text-align: left;
+        margin-bottom: 20px;
+    }
+</style>
--- src/views/SubmitTopic.vue
+++ src/views/SubmitTopic.vue
@@ -1,23 +1,30 @@
 <template>
     <div class="main">
         <div class="container">
+            <h2>发表帖子</h2>
             <div class="topic_title">
-                <Input size="large" placeholder="large size" />
+                <Input size="large" placeholder="请输入标题" style="width: 60%;"/>
+                <span>还可输入80个字符</span>
             </div>
-            <div>
-
+            <div class="topic_content">
+                <editor label="发表帖子"></editor>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Editor from '../components/Editor'
     export default {
-        name: "SubmitTopic"
+        name: "SubmitTopic",
+        components: {Editor},
+        comments: {
+            Editor
+        }
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
     .main {
         width: 100%;
         border-radius: 10px;
@@ -25,13 +32,24 @@
     }
     .container {
         width: 95%;
-        height: 600px;
-        padding: 20px 30px;
-        margin: 50px auto;
-        background-color: skyblue;
+        padding: 10px 30px;
+        margin: 20px auto;
     }
     .topic_title {
-        width: 60%;
-
+        width: 100%;
+        margin-top: 20px;
+        Input {
+            float: left;
+        }
+        span {
+            float: right;
+            font-size: 1.15em;
+            margin-right: 26.5%;
+            line-height: 38px;
+        }
+    }
+    .topic_content {
+        width: 100%;
+        margin-top: 15px;
     }
 </style>
