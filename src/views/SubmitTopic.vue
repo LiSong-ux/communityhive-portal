9d706ef25d874e045ed3e56aa92d6e29b78f37e3
李嵩 <lisong@pansoft.com>
2019-10-29 09:30:20 +0800

    发帖页面新增标签输入框

--- src/views/SubmitTopic.vue
+++ src/views/SubmitTopic.vue
@@ -2,9 +2,14 @@
     <div class="main">
         <div class="container">
             <h2>发表帖子</h2>
-            <div class="topic_title">
-                <Input size="large" placeholder="请输入标题" style="width: 60%;"/>
-                <span>还可输入80个字符</span>
+            <div class="topic_top">
+                <div class="topic_label">
+                    <Input size="large" placeholder="四字标签" style="width: 86px;"/>
+                </div>
+                <div class="topic_title">
+                    <Input size="large" placeholder="请输入标题" style="width: 60%;"/>
+                    <span>还可输入80个字符</span>
+                </div>
             </div>
             <div class="topic_content">
                 <editor label="发表帖子"></editor>
@@ -35,16 +40,25 @@
         padding: 10px 30px;
         margin: 20px auto;
     }
-    .topic_title {
+    .topic_top {
         width: 100%;
         margin-top: 20px;
+    }
+    .topic_label {
+        width: 20%;
+        display: inline;
+    }
+    .topic_title {
+        width: 80%;
+        margin-left: 15px;
+        display: inline;
         Input {
-            float: left;
+            display: inline;
         }
         span {
-            float: right;
+            display: inline;
             font-size: 1.15em;
-            margin-right: 26.5%;
+            margin-left: 15px;
             line-height: 38px;
         }
     }
